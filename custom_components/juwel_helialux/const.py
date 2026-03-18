01c036c264f9a7f0b7f306cfc5941420bc9c013f
Eitschman <eitschman@nebelreich.de>
2026-03-18 09:39:00 +0100

    feat: add LED channel configuration constants
    Add CONF_LED_CHANNELS constant and two mode identifiers
    (LED_CHANNELS_4 for RGBW, LED_CHANNELS_2 for White+Blue)
    to support older HeliaLux LED bars with only 2 channels.

--- custom_components/juwel_helialux/const.py
+++ custom_components/juwel_helialux/const.py
@@ -6,3 +6,8 @@ CONF_TANK_HOST = "tank_host"
 CONF_TANK_NAME = "tank_name"
 CONF_TANK_PROTOCOL = "tank_protocol"
 CONF_UPDATE_INTERVAL = "update_interval"
+CONF_LED_CHANNELS = "led_channels"
+
+# LED channel modes
+LED_CHANNELS_4 = "4ch_rgbw"
+LED_CHANNELS_2 = "2ch_wb"
